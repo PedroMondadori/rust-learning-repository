65ab16bd5f82564433896250f630cc52c0d2dee8
Pedro Mondadori <plmondadori@gmail.com>
2023-07-14 20:14:24 -0300

    Add chapter 14

---
+++ ch14_cargo_crates_io/workspaces/add_two/src/lib.rs
@@ -0,0 +1,16 @@
+use rand;
+
+pub fn add_two(x: i32) -> i32 {
+    return x + 2;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, add_two(2));
+    }
+}
+
